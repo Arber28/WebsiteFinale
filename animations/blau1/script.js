dc0f6c6a446acac97c2a105e063bf986761e5eb2
Arber <82686423+Arber28@users.noreply.github.com>
2026-01-08 14:22:08 +0100

    script.js aktualisieren

--- animations/blau1/script.js
+++ animations/blau1/script.js
@@ -4,11 +4,3 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
-
-// Dynamische Höhe in CSS-Variable speichern
-function setVh() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
-
-window.addEventListener('resize', setVh);
-setVh();
